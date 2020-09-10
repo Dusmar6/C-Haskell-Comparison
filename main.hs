561e8b349b85d5739e8544f0e2b61d4e9ddf4e50
juliasoc <55817015+juliasoc@users.noreply.github.com>
2020-09-10 13:31:14 -0700

    Added comments to quicksort (Haskell)

--- main.hs
+++ main.hs
@@ -5,19 +5,31 @@ main :: IO ()
 main =
   [4, 65, 2, -31, 0, 99, 2, 83, 782, 1] & quickSort & show & putStrLn
 
-
+-- quicksort transforms list of a elements into list of same type
+-- a is an instance of typeclass Ord (elements can be compared)
 quickSort :: (Ord a) => [a] -> [a]
-quickSort []  = []
-quickSort [x] = [x]
+quickSort []  = [] -- empty list
+quickSort [x] = [x] -- necessary?
+-- nonempty list xs where left of pivot is compared to pivotFor
+-- 		then compared with right of pivot
 quickSort xs  = quickSort lhs <> [pivot] <> quickSort rhs
   where
+	-- sets the pivot, xs (rest) for nonempty list xs using xs as parameter for pivotFor
     (pivot, rest) = pivotFor xs
+	-- sets the nonempty lists for left of pivot and right of pivot using pivot, rest as parameters for partition
     (lhs, _, rhs) = partition pivot rest
 
+-- partition transforms a element and list of a elements into ([a], a [a]) of same type
+-- a is an instance of typeclass Ord (elements can be compared)
 partition :: (Ord a) => a -> [a] -> ([a], a, [a])
+-- assigns left of pivot, pivot, right of pivot using parameters pivot and xs (remaining elements)
 partition pivot xs = (lhs, pivot, rhs)
   where
+	-- sets lhs (left of pivot) to values that satisfy predicate (less than pivot) from elements of xs
+	-- sets rhs (right of pivot) to values that do not satisfy predicate from elements of xs
     (lhs, rhs) = List.partition (< pivot) xs
 
+-- pivotFor transforms of list of a elements into (a, [a]) of same type
 pivotFor :: [a] -> (a, [a])
+-- assigns head element x as pivot and remaining elements as rest
 pivotFor (x:rest) = (x, rest)

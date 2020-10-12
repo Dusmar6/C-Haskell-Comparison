02041d075692efdec45cef21c34282c93931bf14
Cullan Oberle <47399807+pcoberle@users.noreply.github.com>
2020-10-12 14:21:22 -0700

    Update lab2_haskell.hs

--- Lab2/lab2_haskell.hs
+++ Lab2/lab2_haskell.hs
@@ -9,6 +9,7 @@ main = print ""
 myFloatList = [645.32, 37.40, 76.30, 5.40, -34.23, 1.11, -34.94, 23.37, 635.46,
   -876.22, 467.73, 62.26]
 
+--Defining a Person data type with a String (name) and an Int (age)
 data Person = Person String Int deriving (Show, Eq)
 
 -- A list of unsorted Persons.
@@ -19,24 +20,30 @@ people = [Person "Hal" 20, Person "Susann" 31, Person "Dwight" 19,
   Person "Risa" 24, Person "Benny" 28, Person "Juan" 33,
   Person "Natalie" 25]
 
+--Fuction for getting the name of a Person.
 name :: Person -> String
-name (Person n _) = n
+name (Person n _) = n -- Ignore the second param and return the first (name)
 
+--Fuction for getting the age of a Person.
 age :: Person -> Int
-age (Person _ a) = a
+age (Person _ a) = a -- Ignore the First param and return the second (age)
 
+--How order is defined on a Person.
 instance Ord Person where
-  p <= q = name p <= name q
+  p <= q = name p <= name q -- Define order of a Person by name (lexicographically)
 
+--Special comparison function for a Person type that orders by age first (decending)
+--then name (lexicographically) if the ages are equal.
 myCompare :: Person -> Person -> Ordering
 myCompare x y
-    | age x < age y = GT
+    | age x < age y = GT --fliped LT and GT to get decending order in sort
     | age x > age y = LT
-    | x > y = GT
+    | x > y = GT  --if age are equal compare by the normal Ord of Person
     | otherwise = LT
 
 
---- shortcuts for the 3 sorts for lab2
+
+--- shortcuts for the 3 sorts for lab2 (sort x == sortBy compare x)
 generic_sort1 = sort myFloatList
 generic_sort2 = sort people
 generic_sort3 = sortBy myCompare people
